4f971748abce2ef947836240201a8a3bba342f92
Devmadia <agathaphan@icloud.com>
2020-07-06 22:58:26 -0500

    developing search functions and savedlist variables for localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,39 @@
-/*
-
-// javascript code for application
-
-*/
-
+// array for storing searchTerms
 var searchList = [];
 
-var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?' +
-          'q=cats&' +
-          newsKL;
-var req = new Request(url);
+var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=';
 
-function getNews(url) {
-    fetch(req)
-    .then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        })
-    })
+// search button activation function
+$("#search-button").on("click", function() {
+    // cancels the event if it is cancelable
+    event.preventDefault(); 
+    var searchTerm = $("#searchField").val();
+    if (searchTerm === ''){ // user does not enter or pick a search term
+        alert("You must enter a search term!");
+    }
+    $("#searchField").val(""); // clears the field after user successfully enters a search term
+    getNews(searchTerm)
+})
+
+// save searched terms to local storage for retrieval
+function saveArticle(news) {
+    // adds new items to the end of an array and returns the new length
+    searchList.push(news);
+    // assigning saved search terms under the key 'userInput' within the array called searchList
+    localStorage.setItem('userInput', JSON.stringify(searchList));
 }
 
-getNews()
+// creating the searched terms' list
+function addArticle(news) {
+    // introduces a list item element
+    var listArticle = $("<li>").addClass("list-group-item list-group-item-action").text(article);
+    // adds the searched term to the ul with a class of saved-articles
+    $("#saved-articles").append(listArticle);
+    saveArticle(news);
+}
 
- /*   /*fetch(url).then(function(response) {
+function getNews(searchTerm) {
+    fetch(url).then(function(response) {
         if (response.ok) {
             response.json()
             .then(function(response) {
@@ -32,7 +43,13 @@ getNews()
                 
             })
         }
-    })*/
+    })
+}
+
+getNews()
+
+
+ /*   /**/
 /*
 
 
@@ -42,10 +59,12 @@ function addArticle(news) {
     saveArticle(news);
 }
 
-function saveArticle(news) {
-    searchList.push(news);
-    localStorage.setItem('userInput', JSON.stringify(searchList));
-}
+fetch(url + searchTerm + '&' + newsKL)
+    .then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    })
 
 function loadArticle() {
     var storedArticle = localStorage.getItem('userInput');
