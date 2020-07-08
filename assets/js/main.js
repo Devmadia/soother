c4e4c9ed831fc9752d660afdcf27a9050fe27539
Devmadia <agathaphan@icloud.com>
2020-07-08 03:57:24 -0500

    working on filling elements with called data, current error exists with data not being pulled from specific array, but overall array of all searched articles

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,9 +23,7 @@ $("#search-button").on("click", function() {
 
 // gathering the news for searched term and storing searched term in an array to call on later
 function getNews(searchTerm) {
-    // let articlesRetrieved = response.docs;
-
-
+    // fetch news based on searchTerm entered by user through on-click input
     fetch(url + searchTerm + '&' + newsKL).then(function(response) {
         if (response.ok) {
             response.json()
@@ -36,28 +34,58 @@ function getNews(searchTerm) {
                         // if image is available, push to articles array
                         if (response.response.docs[i].multimedia.length > 0) { 
                             articles.push(
-                                {"headline" : response.response.docs[i].headline.main, "image" : response.response.docs[i].multimedia[0].url, "url" : response.response.docs[i].web_url});
+                                {"headline" : response.response.docs[i].headline.main, 
+                                "image" : response.response.docs[i].multimedia[0].url, 
+                                "url" : response.response.docs[i].web_url}
+                            );
+
                             console.log(response.response.docs[i].headline.main + response.response.docs[i].multimedia[0].url + response.response.docs[i].web_url);
                         }
                         
                         // if image is not available, push to noImgArt array
                         else { 
                             noImgArt.push(
-                                {"headline" : response.response.docs[i].headline.main, "url" : response.response.docs[i].web_url});
+                                {"headline" : response.response.docs[i].headline.main, 
+                                "url" : response.response.docs[i].web_url}
+                            );
+
                             console.log(response.response.docs[i].headline.main + response.response.docs[i].web_url);
                         }
                     }
                 }
-                         
+                
+                /* current error persists with the fact this is generated 
+                from the entire searchList, not from a specific array made with the image check */ 
+
                 // generates information for first article element
-                var title = response.response.docs[0].headline.main;
-                $("#newsLead").text(response.response.docs[0].lead_paragraph);
-                var link = response.response.docs[0].web_url;
-                var newsImage = response.response.docs[0].multimedia[0].url;
-                var image = $("<img>").attr("src", "https://nytimes.com/" + newsImage);
-                $("#newsOne").append(image);
-                $("#titleOne").append(title);
-                $("#urlOne").append(link);
+                var titleOne = response.response.docs[0].headline.main;
+                // $("#newsLead").text(response.response.docs[0].lead_paragraph);
+                var linkOne = response.response.docs[0].web_url;
+                var newsImageOne = response.response.docs[0].multimedia[0].url;
+                var imageOne = $("<img>").attr("src", "https://nytimes.com/" + newsImageOne);
+                $("#newsOne").append(imageOne);
+                $("#titleOne").append(titleOne);
+                $("#urlOne").append(linkOne);
+
+                // generates information for second article element
+                var titleTwo = response.response.docs[1].headline.main;
+                // $("#newsLead").text(response.response.docs[1].lead_paragraph);
+                var linkTwo = response.response.docs[1].web_url;
+                var newsImageTwo = response.response.docs[1].multimedia[0].url;
+                var imageTwo = $("<img>").attr("src", "https://nytimes.com/" + newsImageTwo);
+                $("#newsTwo").append(imageTwo);
+                $("#titleTwo").append(titleTwo);
+                $("#urlTwo").append(linkTwo);
+
+                // generates information for three article element
+                var titleThree = response.response.docs[2].headline.main;
+                // $("#newsLead").text(response.response.docs[1].lead_paragraph);
+                var linkThree = response.response.docs[2].web_url;
+                var newsImageThree = response.response.docs[2].multimedia[0].url;  // third article does not have an image, thus an error begins
+                var imageThree = $("<img>").attr("src", "https://nytimes.com/" + newsImageThree);
+                $("#newsThree").append(imageThree);
+                $("#titleThree").append(titleThree);
+                $("#urlThree").append(linkThree);
             })
         }
     })
