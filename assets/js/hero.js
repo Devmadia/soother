ee555df44eb3222e9eb68bb0e3ce1f060118686f
Jenniffer <jenlpac@yahoo.com>
2020-07-09 18:06:02 -0500

    add timer to change quote every 5 minutes

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -4,15 +4,23 @@ var html = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=j
 // Create function for random quote
 var getQuote=function(data){
   console.log(data);
+  if(data.quoteAuthor === "") {
+      data.quoteAuthor = "Unknown";
+  }
  
   $('#quote').text(data.quoteText);
   $('#author').text(data.quoteAuthor);
 };
 
-
-  
-// Pull quote into hero
 $.getJSON(html, getQuote, 'jsonp');
+  
+// Pull new quote into hero every 5 minutes
+var newQuote = setInterval(function() {
+    $.getJSON(html, getQuote, 'jsonp');
+}, 60000);
+
+
+
 
 
 
