b313f3f0179a9282b9bb3e634f98bcf8feca2241
Devmadia <agathaphan@icloud.com>
2020-07-09 00:56:24 -0500

    reduced lines by deleting unnecessary functions, redefined comments for ease of reading and following logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,30 @@ var searchList = []; // array to save article titles that persist on refresh bas
 
 var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=';
 
-// search button activation function
+// search field input function
 $("#searchField").keypress("click", function(e) {
 
     if (e.which == 13) {
-       // cancels the event if it is cancelable
-    event.preventDefault(); 
-    var searchTerm = $("#searchField").val();
-    if (searchTerm === ''){ // user does not enter or pick a search term
-        alert("You must enter a search term!");
-    }
-    $("#searchField").val(""); // clears the field after user successfully enters a search term
-    // addArticle(searchTerm)
-    getNews(searchTerm)
-    }
+
+        // cancels the event if it is possible
+        event.preventDefault(); 
+
+        var searchTerm = $("#searchField").val();
+
+        // when user does not enter or pick a search term
+        if (searchTerm === '') { 
+            alert("You must enter a search term!");
+        }
+
+        // clears the field after user successfully enters a search term
+        $("#searchField").val(""); 
+
+        // addArticle(searchTerm)
+        getNews(searchTerm)
+        }
 })
 
-// gathering the news for searched term and storing searched term in an array to call on later
+/* gathers news for user search topic and stors searched term in an array for later usage */
 function getNews(searchTerm) {
 
     // fetch news based on searchTerm entered by user through on-click input
@@ -27,8 +34,11 @@ function getNews(searchTerm) {
         if (response.ok) {
             response.json()
             .then(function(response) {
+
+                // retrieves information from searchList, if there are articles found, it begins to sort them
                 if (!searchList.includes(response.response.docs)) {
-                    // clears arrays before for-loop is run
+
+                    /* clears arrays before for-loop is utilized */
                     var articles = [];
                     var noImgArt = [];
 
@@ -37,16 +47,19 @@ function getNews(searchTerm) {
 
                         // if image is available, push to articles array
                         if (response.response.docs[i].multimedia.length > 0) { 
+
+                            // if images are found in article, it is pushed to articles array
                             articles.push(
                                 {"headline" : response.response.docs[i].headline.main, 
                                 "image" : response.response.docs[i].multimedia[0].url, 
                                 "url" : response.response.docs[i].web_url}
                             );
-
-                            console.log(response.response.docs[i].headline.main + response.response.docs[i].multimedia[0].url + response.response.docs[i].web_url);
+                            
+                            /* for debugging purposes */
+                            //console.log(response.response.docs[i].headline.main + response.response.docs[i].multimedia[0].url + response.response.docs[i].web_url);
                         }
                         
-                        // if image is not available, push to noImgArt array
+                        // if image is not available, they're pushed to noImgArt array
                         else { 
                             noImgArt.push(
                                 {"headline" : response.response.docs[i].headline.main, 
@@ -54,9 +67,12 @@ function getNews(searchTerm) {
                                 "abstract" : response.response.docs[i].abstract}
                             );
 
-                            console.log(response.response.docs[i].headline.main + response.response.docs[i].web_url);
+                            /* for debugging purposes */
+                            //console.log(response.response.docs[i].headline.main + response.response.docs[i].web_url);
                         }
                     }
+
+                    // provides information for the #newsResults & #otherNews elements
                     makeCard(articles, noImgArt)
                 }
             })
@@ -64,23 +80,23 @@ function getNews(searchTerm) {
     })
 }
 
-// makeCard(articles, noImgArt) : this passes articles from the getNews articles array to the function below
+/* "makeCard(articles, noImgArt):" passes (articles, noImgArt) from the getNews 
+articles array to the function below */ 
+
 function makeCard(articles, noImgArt) {
 
-    // clears div contents
+    /* clears contents of divs with article data rendered */  
     $("#newsResults").empty();
     $("#otherResults").empty();
-    /* current error persists with the fact this is generated 
-    from the entire searchList, not from a specific array made with the image check */ 
+
+    /* retrieves articles array information */ 
 
     for (var i = 0; i < articles.length; i++) {
                     
-        // if image is available in article, which should be as it retrieves from the articles array
-        
         // generates information for first article element
         var titleOne = articles[i].headline;
         
-        // opens target link in new window
+        // opens target link in new window to avoid copyright issues
         var titleLink = $("<a>").attr("target", "_blank");
 
         // retrieves titleLinkfrom the variable, thus remains set to [0]
@@ -88,8 +104,6 @@ function makeCard(articles, noImgArt) {
         titleLink.html(titleOne);
         var newsImageOne = articles[i].image;
         var image = $("<img>").attr("src", "https://nytimes.com/" + newsImageOne);
-        // $("#image").append(imageOne);
-        // $("#titleOne").append(titleLink); // target link open in new tab
         var insertImage = $("<p>").attr('id', 'image');
         var insertTitle = $("<p>").attr('id', 'title');
         var innerBox = $("<div>").addClass("primary-callout callout results");
@@ -101,12 +115,16 @@ function makeCard(articles, noImgArt) {
         $("#newsResults").append(outerBox);
     }
     
+    /* notification on page within element for when no articles are returned without images */
     if (noImgArt.length == 0) {
         $("#otherResults").append("No results without images found.");
     }
+
     else { 
+
+        // populates information in an RSS feed style for articles without images
         for (var i = 0; i < noImgArt.length; i++) {
-            // 
+             
                 var titleOne = noImgArt[i].headline;
 
                 // opens target link in new window
@@ -115,42 +133,34 @@ function makeCard(articles, noImgArt) {
                 // retrieves titleLinkfrom the variable, thus remains set to [0]
                 titleLink[0].href=noImgArt[i].url;
                 titleLink.html(titleOne);
-                // var newsImageOne = noImgArt[i].image;
-                // var image = $("<img>").attr("src", "https://nytimes.com/" + newsImageOne);
-                // $("#image").append(imageOne);
-                // $("#titleOne").append(titleLink); // target link open in new tab
-                // var insertImage = $("<p>").attr('id', 'image');
                 var insertTitle = $("<p>").attr('id', 'title');
                 var insertAbstract = $("<p>").attr('id', 'abstract');
-                // var outerBox = $("<div>").addClass("large-4 medium-4 small-4 cell");
                 insertTitle.append(titleLink);
-                // insertImage.append(image);
                 insertAbstract.append(noImgArt[i].abstract);
-                // outerBox.append(innerBox);
                 $("#otherResults").append(insertTitle, insertAbstract);
         }
     }
 }
 
-// save searched terms to local storage for retrieval
-function saveArticle(news) {
-    // adds new items to the end of an array and returns the new length
-    searchList.push(news);
-    // assigning saved search terms under the key 'userInput' within the array called searchList
-    localStorage.setItem('userInput', JSON.stringify(searchList));
-}
+// // save searched terms to local storage for retrieval
+// function saveArticle(news) {
+//     // adds new items to the end of an array and returns the new length
+//     searchList.push(news);
+//     // assigning saved search terms under the key 'userInput' within the array called searchList
+//     localStorage.setItem('userInput', JSON.stringify(searchList));
+// }
 
-function loadArticle() {
-    var storedArticle = localStorage.getItem('userInput');
-    storedArticle = JSON.parse(storedArticle);
+// function loadArticle() {
+//     var storedArticle = localStorage.getItem('userInput');
+//     storedArticle = JSON.parse(storedArticle);
 
-    if(!storedArticle) {
-        return false;
-    }
+//     if(!storedArticle) {
+//         return false;
+//     }
 
-    for(var i=0; i <storedArticle.length; i++) {
-        addArticle(storedArticle[i]);
-    }
-}
+//     for(var i=0; i <storedArticle.length; i++) {
+//         addArticle(storedArticle[i]);
+//     }
+// }
 
-loadArticle()
+// loadArticle()
