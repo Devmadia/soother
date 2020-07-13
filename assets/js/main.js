f76b03dd92fda5482c712a38461caa781261253a
Anne <anne.k.plummer@gmail.com>
2020-07-12 20:30:10 -0500

    mobile version now cascades to one search result box per line

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,7 +122,7 @@ function makeCard(articles, noImgArt) {
         $(saveBtn).on("click", getArticle);
 
         var innerBox = $("<div>").addClass("primary-callout callout results card-size  grid-x grid-margin-x");
-        var outerBox = $("<div>").addClass("large-4 medium-4 small-4 cell");
+        var outerBox = $("<div>").addClass("large-4 cell");
         insertTitle.append(titleLink);
         insertImage.append(image);
         innerBox.append(insertImage, insertTitle, saveBtn);
